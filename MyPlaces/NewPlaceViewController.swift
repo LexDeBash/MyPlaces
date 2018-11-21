d4b1d1a126d4aab52e00bceb50d60a3d464a48df
Alexey Efimov <lex.efimov@gmail.com>
2018-11-21 17:32:22 +0500

    Design for NewPlaceViewController

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Alexey Efimov on 21/11/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    // Скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
