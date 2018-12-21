b8cc2c3a6e8c43b86ceb433fd888c74890d50a45
Alexey Efimov <lex.efimov@gmail.com>
2018-12-21 16:11:11 +0500

    1. Remove images for places 2. Refactor code to use DB

---
+++ MyPlaces/StorageManager.swift
@@ -0,0 +1,21 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Alexey Efimov on 07/12/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func saveObject(_ place: Place) {
+        
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+}
